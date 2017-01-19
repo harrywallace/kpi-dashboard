b4d9a76dd045c550d9d004d8c0d6882a2a76b661
Harry Wallace <hfwallace06@gmail.com>
2017-01-19 12:18:05 -0600

    add deploy command and set env to production for node

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,12 @@ module.exports = {
         }]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
 };
