2c02bbb28b9724d15cbb81e35f33f867352fd912
Harry Wallace <hfwallace06@gmail.com>
2017-01-19 12:58:26 -0600

    turns out those configs are unnecessary, who knew

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,5 @@ module.exports = {
         }]
       }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
+  }
 };
