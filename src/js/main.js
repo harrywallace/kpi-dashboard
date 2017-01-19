65bba265fdf8eb60abfe8d93ed977f617b800dd1
Harry Wallace <hfwallace06@gmail.com>
2017-01-18 22:25:34 -0600

    use webpack and npm to handle angular and firebase dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
-/**
- * Main AngularJS Web Application
- */
+'use strict';
+
+var angular = require('angular');
+require('angular-route');
+
+var firebase = require('firebase');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCittNEnyew7n3TEB9RKKdCsHynlV6dO1M",
+  authDomain: "myfinance-kpi-dashboard.firebaseapp.com",
+  databaseURL: "https://myfinance-kpi-dashboard.firebaseio.com",
+  storageBucket: "myfinance-kpi-dashboard.appspot.com",
+  messagingSenderId: "510199120451"
+};
+firebase.initializeApp(config);
+
 var app = angular.module('dashboard', [
   'ngRoute'
 ]);
