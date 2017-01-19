a5966557e5223b86819c255fd20e3097c61396af
Harry Wallace <hfwallace06@gmail.com>
2017-01-18 21:53:52 -0600

    split up src and public, try bundling

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
+  context: path.resolve(__dirname, './src/js'),
   entry: {
     main: './main.js'
   },
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, './public/assets'),
+    publicPath: '/assets',
     filename: '[name].bundle.js'
   }
 };
