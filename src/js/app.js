fe9e80191af08040d523b82521f7550fad0b17b8
Harry Wallace <hfwallace06@gmail.com>
2017-01-24 18:55:41 -0600

    small tweak

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import firebase from 'firebase';
+import config from './firebaseConfig';
 
 // Initialize Firebase
-const config = {
-  apiKey: "AIzaSyCittNEnyew7n3TEB9RKKdCsHynlV6dO1M",
-  authDomain: "myfinance-kpi-dashboard.firebaseapp.com",
-  databaseURL: "https://myfinance-kpi-dashboard.firebaseio.com",
-  storageBucket: "myfinance-kpi-dashboard.appspot.com",
-  messagingSenderId: "510199120451"
-};
 firebase.initializeApp(config);
 
 class MapState extends React.Component {
